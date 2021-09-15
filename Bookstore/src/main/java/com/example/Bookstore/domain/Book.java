7928f207c1d8a8b7164c3779514df426cee054fd
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-09-15 14:47:00 +0300

    Add files via upload

--- Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,13 +1,28 @@
 package com.example.Bookstore.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;	
+	private String author;	
+	private int year;	
+	private String isbn;
+	private double price;
 	
-	public String title;	
-	public String author;	
-	public int year;	
-	public String isbn;
-	public double price;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 	public String getTitle() {
 		return title;
@@ -49,8 +64,23 @@ public class Book {
 		this.price= price;	
 	}
 	
+	public Book() {}
+	
+	public Book(Long id, String title, String author, int year, String isbn, double price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
 	@Override
 	public String toString() {
-		return "Book [title=" + title + "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
+
+
 }
